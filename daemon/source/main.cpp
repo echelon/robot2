151d04c4ac0ffe0568fc837023b052b015c473c1
Brandon Thomas Suit <brandon.suit@gmail.com>
2012-06-25 10:32:38 -0400

    C++ cleanup

--- daemon/source/main.cpp
+++ daemon/source/main.cpp
@@ -5,47 +5,25 @@
 #include <iostream>
 #include <stdexcept>
 #include <stdio.h> // TODO: Remove
-#include <time.h> // Get system time, sleep()
+#include <time.h> // Get system time
 #include <cstdlib>
 #include <cstring> // TODO: Ugh, remove
 #include "Serial.hpp"
 
 /**
- * Try to open one of the first ten serial ports.
+ * Constants
  */
-Serial* openSerial()
-{
-	Serial* s = 0;
-	
-	for(int i = 0; i < 10; i++) {
-		s = new Serial(i);
-		try {
-			std::cout << "Opening serial on #" << i << std::endl;
-			s->open();
-		}
-		catch(const std::runtime_error& e) {
-			std::cerr << "Could not open serial on #" << i << std::endl;
-			delete s;
-			s = 0;
-			continue;
-		}
-		break;
-	}
-	return s;
-}
-
-// FIXME: No globals! 
-Serial* robot = 0;
+const int MSECONDS_TIMEOUT = 200;
 
 /**
- * Stuff to keep track of commands and timeouts.
+ * Nasty globals
  */
-enum LastCommand { 
-	COMMAND_FROM_NETWORK, 
-	COMMAND_FROM_TIMEOUT
-};
+Serial* robot = 0;
 
-LastCommand lastCommandType = COMMAND_FROM_TIMEOUT;
+// These are for communicating between threads.
+pthread_mutex_t messageMut = PTHREAD_MUTEX_INITIALIZER;
+std::string messageStr = "";
+timespec messageTime;
 
 /**
  * See if a number of milliseconds has elapsed since the timestamp.
@@ -66,16 +44,11 @@ bool msec_elapsed(timespec then, int ms)
 }
 
 /**
- * Newer shared stuff.
- */
-pthread_mutex_t messageMut = PTHREAD_MUTEX_INITIALIZER;
-std::string messageStr = "";
-timespec messageTime;
-
-
-/**
- * Send a robot a simple motor command.
+ * Send a robot a _simple_ motor command.
  * Inputs are 'wasd' and 'e'. 
+ * TODO: I have to do more sophisticated than this; 
+ *       such as speed differentials
+ * FIXME: relies on global
  */
 void robot_send_command(const char* msg)
 {
@@ -100,8 +73,8 @@ void robot_send_command(const char* msg)
 	if(command.length()) {
 		robot->write(command);
 	}
-
 }
+
 /**
  * Thread that responds to ZMQ messages and relays them 
  * to the robot.
@@ -151,9 +124,11 @@ void* ZmqServerThread(void* n)
  */
 void* TimeThread(void* n)
 {
-	int MSECONDS_TIMEOUT = 200;
 	timespec lastWritten_command;
 	timespec lastWritten_timeout;
+
+	enum LastCommand { COMMAND_FROM_NETWORK, COMMAND_FROM_TIMEOUT };
+	LastCommand lastCommandType = COMMAND_FROM_TIMEOUT;
 	std::string lastCommand("");
 
 	while(1)
@@ -161,6 +136,7 @@ void* TimeThread(void* n)
 		timespec t;
 		std::string m;
 
+		// Any commands from the network? 
 		pthread_mutex_lock(&messageMut);
 		t = messageTime;
 		m = messageStr;
@@ -168,34 +144,27 @@ void* TimeThread(void* n)
 
 		// Timeouts.
 		if(msec_elapsed(t, MSECONDS_TIMEOUT)) {
+			// Don't flood serial unless timeout is a new event
 			if(lastCommandType != COMMAND_FROM_TIMEOUT || 
 					msec_elapsed(lastWritten_timeout, 100)) {
-				lastCommandType = COMMAND_FROM_TIMEOUT;
 				robot_send_command("e");
 				clock_gettime(CLOCK_REALTIME, &lastWritten_timeout);
-				std::cout << "timeout" << std::endl;
+				lastCommandType = COMMAND_FROM_TIMEOUT;
 			}
 			continue;
 		}
 
 		// Commands.
 		if(m.length()) {
-
+			// Don't flood serial
 			if(msec_elapsed(lastWritten_command, 100)) {
-
-				std::cout << "messg if (time)" << std::endl;
-
 				robot_send_command(m.c_str());
 				clock_gettime(CLOCK_REALTIME, &lastWritten_command);
 			}
+			// Ensure new commands get written
 			else if(lastCommand.compare(m)) {
-
-				std::cout << "messg elseif (compare str)" << std::endl;
-
 				robot_send_command(m.c_str());
 				clock_gettime(CLOCK_REALTIME, &lastWritten_command);
-			}
-			else {
 			}
 			lastCommand = m;
 			lastCommandType = COMMAND_FROM_NETWORK;
@@ -203,6 +172,31 @@ void* TimeThread(void* n)
 	}
 
 	pthread_exit(0);
+}
+
+/**
+ * Try to open one of the first ten serial ports.
+ * It's probably the first serial device (0), but just in case...
+ */
+Serial* openSerial()
+{
+	Serial* s = 0;
+	
+	for(int i = 0; i < 10; i++) {
+		s = new Serial(i);
+		try {
+			std::cout << "Opening serial on #" << i << std::endl;
+			s->open();
+		}
+		catch(const std::runtime_error& e) {
+			std::cerr << "Could not open serial on #" << i << std::endl;
+			delete s;
+			s = 0;
+			continue;
+		}
+		break;
+	}
+	return s;
 }
 
 /**
@@ -221,12 +215,11 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 
-	zmqThread  = pthread_create(&zmqThread, 0, ZmqServerThread, 0);
-	timeThread = pthread_create(&timeThread, 0, TimeThread, 0);
+	pthread_create(&zmqThread, 0, ZmqServerThread, 0);
+	pthread_create(&timeThread, 0, TimeThread, 0);
 
-	while(1) {
-		//wait(1);
-	}
+	pthread_join(zmqThread, 0);
+	pthread_join(timeThread, 0);
 
 	return EXIT_SUCCESS;
 }
